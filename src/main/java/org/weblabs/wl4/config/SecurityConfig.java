e611fb5088714a79cd2613c0d178752a04dcb371
tanutamonfish <tanutamonfish@gmail.com>
2025-12-23 00:23:36 +0300

    update: frontend

--- src/main/java/org/weblabs/wl4/config/SecurityConfig.java
+++ src/main/java/org/weblabs/wl4/config/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authz -> authz
                 .requestMatchers("/api/v1/auth/**").permitAll()
-                .requestMatchers("/*").permitAll()
+                .requestMatchers("/api/**").authenticated()
+                .requestMatchers("/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
