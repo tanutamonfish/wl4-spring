a52493ad5adc4db42f40bb4c1838b098e60f4df6
tanutamonfish <noemail@example.com>
2025-12-23 09:27:06 +0300

    add: validation email

--- src/main/java/org/weblabs/wl4/dto/AuthRequest.java
+++ src/main/java/org/weblabs/wl4/dto/AuthRequest.java
@@ -2,12 +2,14 @@ package org.weblabs.wl4.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AuthRequest {
     @Email
     @NotBlank
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message = "Email should be valid")
     private String email;
     @NotBlank
     private String password;
