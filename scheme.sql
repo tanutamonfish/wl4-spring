790a9f2766bd20aac73d75d59d4ed94cf81d55ac
tanutamonfish <noemail@example.com>
2025-12-29 21:46:23 +0300

    refactor: add NOT NULL to fields (point_checks)

--- scheme.sql
+++ scheme.sql
@@ -6,14 +6,14 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS point_checks (
     id BIGSERIAL PRIMARY KEY,
-    r DOUBLE PRECISION,
-    x DOUBLE PRECISION,
-    y DOUBLE PRECISION,
-    hit BOOLEAN,
-    checked_at TIMESTAMP,
-    execution_time_ms BIGINT,
+    r DOUBLE PRECISION NOT NULL,
+    x DOUBLE PRECISION NOT NULL,
+    y DOUBLE PRECISION NOT NULL,
+    hit BOOLEAN NOT NULL,
+    checked_at TIMESTAMP NOT NULL,
+    execution_time_ms BIGINT NOT NULL,
     user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE INDEX IF NOT EXISTS idx_point_checks_user_id ON point_checks(user_id);
-CREATE INDEX IF NOT EXISTS idx_point_checks_checked_at ON point_checks(checked_at DESC);
+CREATE INDEX IF NOT EXISTS idx_point_checks_checked_at ON point_checks(checked_at);
