dd77673138aa8fc3af87a34836ab6ebbbab22b33
tanutamonfish <noemail@example.com>
2025-12-29 17:22:45 +0300

    fix: unnessesary line

--- src/main/java/org/weblabs/wl4/dto/AuthRequest.java
+++ src/main/java/org/weblabs/wl4/dto/AuthRequest.java
@@ -9,7 +9,6 @@ import lombok.Data;
 public class AuthRequest {
     @Email
     @NotBlank
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message = "Email should be valid")
     private String email;
     @NotBlank
     private String password;
